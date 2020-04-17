8fc3011f76a2cf05911f7f160295d0b4d5c04d4f
amir hoshen <mi.rosh160@gmail.com>
2020-04-17 19:47:12 +0300

    omer FamilyTree.cpp/hpp first version

---
+++ FamilyTree.hpp
@@ -0,0 +1,85 @@
+//
+// Created by miros on 17/04/2020.
+//
+
+#ifndef ANCESTOR_TREE_B_FAMILYTREE_HPP
+#define ANCESTOR_TREE_B_FAMILYTREE_HPP
+
+
+#pragma once
+#include<string>
+#include <iostream>
+using namespace std;
+
+namespace family{
+
+    class Node{
+
+        Node* father;
+        Node* mother;
+        string name;
+
+    public:
+        Node()
+        {
+            name = "";
+            father = NULL;
+            mother = NULL;
+        }
+        Node(string name)
+        {
+            this->name = name;
+            father = NULL;
+            mother = NULL;
+        }
+        ~Node();
+        string getName(){
+            return this->name;
+        }
+        Node* getMother(){
+            return this->mother;
+        }
+        Node* getFather(){
+            return this->father;
+        }
+        void setNode(string parent, bool g) //false = mother, true = father
+        {
+            if(g == true)
+            {
+                Node* ans = new Node(parent);
+                this->father = ans;
+            }
+            else
+            {
+                Node* ans = new Node(parent);
+                this->mother = ans;
+            }
+        }
+    };
+
+    class Tree{
+        Node* root;
+    public:
+
+        Tree(string name)
+        {
+            this->root = new Node(name);
+        }
+        Tree& addFather(string name, string father);
+        Tree& addMother(string name, string mother);
+        string relation(string name);
+        string find(string relation);
+        void display();
+        void remove(string name);
+        void add(Node* root ,string name, string parent,bool g);
+        void printTree(Node* root);
+        Node* findNode(Node* root, string name);
+
+    };
+}
+
+
+
+
+
+#endif //ANCESTOR_TREE_B_FAMILYTREE_HPP
